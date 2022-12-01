8846a959e09652e19ecc8b317fbc7b4a8e6f375c
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-01 12:58:25 -0800

    Added scripts for 0x02. Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+import sys
+
+nargs = len(argv) - 1
+if nargs == 0:
+    print("{} arguments.".format(nargs))
+elif nargs == 1:
+    print("{} argument.".format(nargs))
+    print("{}: {}".format(nargs, argv[1]))
+else:
+    print("{} arguments:".format(nargs))
+    for i in range(1, nargs + 1):
+        print("{}: {}".format(i, argv[i]))
