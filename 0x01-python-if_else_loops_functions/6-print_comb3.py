91e0493fdf53ef75c85e595d2853c6ed83a1d75a
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-01 17:59:24 -0800

    Added scripts for 0x02. Python - import & modules

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+for a in range(48, 58):
+    for b in range(48, 58):
+        if ord(a) == ord(b) or ord(b) <= ord(a):
+            continue
+        else:
+            print("{}{}".format(ord(a), ord(b))
+
+            if (a == 56 && b == 57):
+                continue
+            else:
+                print("{} ".format(ord(44)))
+print("")
