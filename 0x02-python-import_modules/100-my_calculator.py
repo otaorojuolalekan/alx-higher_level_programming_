c49d12fe6096c8d914c5e87efa30fa584a88aa20
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-01 17:43:01 -0800

    Added scripts for 0x02. Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv, exit
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
     from calculator_1 import add, sub, mul, div
-
-nargs = len(argv) - 1
-
-if nargs != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-
-if argv[2] not in "+-*/":
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-
-a = int(argv[1])
-b = int(argv[3])
-op = argv[2]
-
-if op == '+':
-    result = add(a, b)
-elif op == '-':
-    result = sub(a, b)
-elif op =='*':
-    result = mul(a, b)
-elif op == '/':
-    result = div(a, b)
-
-print("{} {} {} = {}".format(a, op, b, result))
-exit(0)
+    funcs = [add, sub, mul, div]
+    for i, s in enumerate(ops):
+        if argv[2] == s:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
