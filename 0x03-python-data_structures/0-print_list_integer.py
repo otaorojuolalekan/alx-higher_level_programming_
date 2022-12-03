9b688f070fd3ece671727e353b36de8d5e8089d6
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-03 12:51:01 -0800

    Added scripts for 0x03-python-data_structures

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+print_list_integer: prints all integers in a list
+my_list: argument for print_list_integer function
+Returns: No return
+"""
+
+
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        print("{:d}.format(item)")
