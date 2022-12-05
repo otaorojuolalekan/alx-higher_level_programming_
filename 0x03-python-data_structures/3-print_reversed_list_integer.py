9678c1f1d83861d8e45ffb88d73496cbab6e116c
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-05 22:53:08 +0100

    Added scripts for 0x03-python-data_structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        print("")
-    elif len(my_list) == 1:
-        print("{:d}".format(my_list[0]))
-    else:
-        list_copy = my_list.copy()
-        list_copy.reverse()
-        for item in list_copy:
-            print("{:d}".format(item))
+    my_list.reverse()
+    for item in my_list:
+        print("{:d}".format(item))
