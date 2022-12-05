0b8c4455348f80c4501fe863585c82ce60c905bb
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-05 20:22:31 +0100

    Added scripts for 0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if 0 > idx > len(my_list):
+    if 0 > idx > len(my_list) - 1:
         return my_list
     else:
-        new_list = [item for item in my_list]
+        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
