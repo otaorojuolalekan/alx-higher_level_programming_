643b044ab88df0ab3705b1425d979bfd604d38a9
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-06 01:17:30 +0100

    Added scripts for 0x03-python-data_structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    listlen = len(my_list)
-    if listlen == 0:
-        return None
-    max1 = my_list[0]
-    for i in range(listlen - 1):
-        if my_list[i] > max1:
-            max1 = my_list[i]
-    return max1
+    if isinstance(my_list, list):
+        listlen = len(my_list)
+        if listlen == 0:
+            return None
+        max1 = my_list[0]
+        for i in range(listlen - 1):
+            if my_list[i] > max1:
+                max1 = my_list[i]
+        return max1
