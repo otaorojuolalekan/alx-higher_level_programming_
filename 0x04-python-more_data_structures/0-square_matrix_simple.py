959f59a1071e38b59e83055fbd55f13ed670003b
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-07 22:08:42 +0100

    Added scripts for 0x04. Python - More Data Structures: Set, Dictionary tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[row[i]**2 for i in range(len(matrix))] for row in matrix]
+    return [list(map(lambda x: x**2, row)) for row in matrix]
