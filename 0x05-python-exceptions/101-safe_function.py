76547eb733142b62e6c93147536f82aa96c41951
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-20 22:29:11 +0100

    Added scripts for 0x05. Python - Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except (TypeError, NameError, ValueError):
+    except (TypeError, NameError, ValueError, ZeroDivisionError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
