9cf4fce5c0c77960b43f85245b59336379f18209
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-19 22:31:00 -0800

    Added scripts for 0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        if x > len(my_list):
+            raise IndexError
+        
+        for i in my_list[0:x]:
+            print(i, end="")
+        return x
+    except IndexError:
+        for i in my_list[0:len(my_list)]:
+            print(i, end="")
+        return len(my_list)
+    finally:
+        print()
