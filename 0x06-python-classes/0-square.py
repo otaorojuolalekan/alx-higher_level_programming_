b8c2f605d0046bd1b2335fbebf55225273577c73
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-28 23:56:35 -0800

    Added scripts for 0x06. Python - Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module Documentation.
+
+This is the first attempt to create a class in python.
+Don't fuss to much about it, this class does nothing
+"""
 
 class Square():
     """This is an empty class that defines a square"""
