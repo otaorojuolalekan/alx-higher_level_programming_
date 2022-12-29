915fdcd1b7c6048bf2ffc9f71d9a41e613a5dcc4
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-29 12:24:47 +0100

    Added scripts for 0x06. Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,9 +7,6 @@ Instantiation with size (no type/value verification)
 You are not allowed to import any module
 Why?
 
-Why size is private attribute?
-
-The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. One way to have the control is to keep it privately. You will see in next tasks how to get, update and validate the size value.
 
 guillaume@ubuntu:~/0x06$ cat 1-main.py
 #!/usr/bin/python3
@@ -34,7 +31,7 @@ guillaume@ubuntu:~/0x06$ ./1-main.py
 {'_Square__size': 3}
 'Square' object has no attribute 'size'
 'Square' object has no attribute '__size'
-guillaume@ubuntu:~/0x06$ 
+guillaume@ubuntu:~/0x06$
 """
 
 
