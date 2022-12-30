7bdd8b052ccc823b0c39eb4ceaecf456357d4466
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-30 21:25:26 +0100

    Added scripts for 0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module Documentation:
+Write a class Square that defines a square by:
+(based on 2-square.py)
+"""
+
+
+class Square():
+    """
+    Write a class Square that defines a square by:
+    (based on 3-square.py)
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
