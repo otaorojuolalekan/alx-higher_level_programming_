da87e22df1dcf3667310d53fd0bc50f5d0fc5bd2
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-12-30 21:06:24 +0100

    Added scripts for 0x06. Python - Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,28 +2,28 @@
 """ Module Documentation:
 Write a class Square that defines a square by:
 (based on 2-square.py)
-Private instance attribute: size
-Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception
-with the message size must be an integer
-if size is less than 0, raise a ValueError exception
-with the message size must be >= 0
-Public instance method: def area(self): that returns
-the current square area
-You are not allowed to import any module
 """
 
 
 class Square():
-    """class Square that defines a square by:
-    (based on 2-square.py)
+    """
+    Write a class Square that defines a square by:
+    (based on 3-square.py)
     """
     def __init__(self, size=0):
         self.__size = size
-        if not isinstance(self.__size, int):
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if self.__size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")        
+        self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        return self.__size * self.__size
